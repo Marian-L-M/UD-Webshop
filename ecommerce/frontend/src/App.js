4d98b308d28d27b0550e77d0e0ef135497d70bf5
MarianMaschkeRGST <m.maschke@rgst.net>
2021-12-29 15:02:36 +0900

    change href to routes

--- ecommerce/frontend/src/App.js
+++ ecommerce/frontend/src/App.js
@@ -1,21 +1,26 @@
 import { Container } from 'react-bootstrap'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Header from './components/Header'
 import Footer from './components/Footer'
 
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
-    <div className="">
+    <Router className="">
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path='/' element={ <HomeScreen /> } exact />
+            <Route path='/product/:id' element={ <ProductScreen /> } />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
